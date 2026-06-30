161a461fee12ad0012306ccd23dc06816d3ba4da
farhad1984crypto <farhad1984crypto@gmail.com>
2026-07-01 00:28:42 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      zlib: 'browserify-zlib',
-      util: 'util',
-    },
-  },
-});
+  ssr: {
+    noExternal: ['@supabase/supabase-js']
+  }
+})
