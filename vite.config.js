ff476d7983b0a5a4c50e90d9b3554467f598f346
farhad1984crypto <farhad1984crypto@gmail.com>
2026-07-01 00:36:04 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
-  plugins: [react()],
-  ssr: {
-    noExternal: ['@supabase/supabase-js']
-  }
+  plugins: [react()]
 })
